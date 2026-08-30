310bfd26a828bf79095a30753382e7bf2e6be01e
eoghanlochlyneoghanlochlyn <eoghanlochlyn@atomicmail.io>
2026-08-30 10:06:32 +0330

    Update image_filters.py

--- image/image_filters.py
+++ image/image_filters.py
@@ -1,1 +1,17 @@
+import html
+import re
 
+from urllib.parse import (
+    parse_qs,
+    unquote,
+    urlsplit,
+)
+
+
+def safe_int(value):
+
+    try:
+        return int(value)
+
+    except Exception:
+        return 0
