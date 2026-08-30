f84ee2105827ef2940144b050d16d1ef4184e461
eoghanlochlyneoghanlochlyn <eoghanlochlyn@atomicmail.io>
2026-08-30 11:14:57 +0330

    Update image_filters.py

--- image/image_filters.py
+++ image/image_filters.py
@@ -60,3 +60,46 @@ def unwrap_image_proxy_url(url):
         pass
 
     return url
+
+def looks_like_thumbnail_url(url):
+
+    if not url:
+        return False
+
+    lower = url.lower()
+
+    patterns = [
+
+        r"-\d{2,4}x\d{2,4}(?:\.[a-z0-9]+)(?:\?|$)",
+
+        r"/\d{2,4}x\d{2,4}/",
+
+        r"[?&]width=(?:120|150|160|180|200|240|300|320|400|480|640)(?:&|$)",
+
+        r"[?&]w=(?:120|150|160|180|200|240|300|320|400|480|640)(?:&|$)",
+
+        r"[?&]width=(?:120|150|160|180|200|240|300|320|400|480|640)[^0-9]",
+
+        r"[?&]w=(?:120|150|160|180|200|240|300|320|400|480|640)[^0-9]",
+    ]
+
+    for pattern in patterns:
+
+        if re.search(pattern, lower):
+            return True
+
+    words = (
+        "thumbnail",
+        "thumb",
+        "small",
+        "tiny",
+        "avatar",
+        "favicon",
+    )
+
+    for word in words:
+
+        if word in lower:
+            return True
+
+    return False
